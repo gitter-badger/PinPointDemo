d561a9512dcfcd0ff21438213f6a39673b16ef3b
stvnksslr <stvnksslr@gmail.com>
2015-02-02 19:45:36 -0500

    formatting(templates:gitignore:pep8): cleaned up some formatting and added some project files to the gitignore. going to begin refactoring the javascript. python files are all looking good just needed to clip trailing spaces and normalize tabs and spaces.

--- app/forms.py
+++ app/forms.py
@@ -2,5 +2,6 @@ from flask.ext.wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
+
 class AddressEntry(Form):
-	address_entry = StringField('address_entry', validators=[DataRequired()])
+    address_entry = StringField('address_entry', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -4,32 +4,33 @@ from .forms import AddressEntry
 from PinPointGeocoder.geocoder.engine import Engine
 import sys
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def demo():
-    form = AddressEntry()     
+    form = AddressEntry()
     results = None
     marker = None
     lat = 51.505
     lon = -0.09
 
     if form.validate_on_submit():
-    	e = Engine()
-    	try:
-    		# print '\tTrying:(%s)' % form.address_entry.data
-    		results = e.geocode(form.address_entry.data)
-		print results # debug->lat/lon is getting rounded...
-    		if results['lat'] and results['lon']: 
-    			marker = "var marker = L.marker([%s, %s]).addTo(map);" % (results['lat'],results['lon'])
-    			lat = results['lat']
-    			lon = results['lon']
-    	except:
-    		results=None
+        e = Engine()
+        try:
+            # print '\tTrying:(%s)' % form.address_entry.data
+            results = e.geocode(form.address_entry.data)
+            print results  # debug->lat/lon is getting rounded...
+            if results['lat'] and results['lon']:
+                marker = "var marker = L.marker([%s, %s]).addTo(map);" % (
+                    results['lat'], results['lon'])
+                lat = results['lat']
+                lon = results['lon']
+        except:
+            results = None
 
     return render_template('address_entry.html',
-    	                    lat = lat,
-    	                    lon = lon,
-                            results = results,
-                            marker = marker,
-                            form = form
-                            )
-
+                           lat=lat,
+                           lon=lon,
+                           results=results,
+                           marker=marker,
+                           form=form
+                           )
