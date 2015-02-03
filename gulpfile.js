682d418624482d31cd4ab346ec6dab6f4ca284cf
stvnksslr <stvnksslr@gmail.com>
2015-02-02 22:02:13 -0500

    improvement(gitingnore:gulpfile): finished build tools the gup file has a single task that will compile sass into css alowing quick and easy manipulation of bootstrap elements without needing to take large amounts of code from examples.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@
  * Created by skessler on 2/2/15.
  */
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass');
+
 
 gulp.task('default', ['styles']);
 
-gulp.task('default', []);
+gulp.task('styles', function() {
+    return gulp.src('./app/app.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(gulp.dest('app/'));
+});
